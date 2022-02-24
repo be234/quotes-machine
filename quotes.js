30c2b74942fdbb6b0cf96658791928c47cfd7738
belalelgebaly <belalelgebaly4@gmail.com>
2022-02-24 19:57:24 +0200

    Done

---
+++ quotes.js
@@ -0,0 +1,255 @@
+const QUOTEBANK = [
+    {
+    quote: "Why is it that when one man builds a wall, the next man immediately needs to know what's on the other side?",
+ author: "Tyrion Lannister"},
+ {
+    quote: "Once you’ve accepted your flaws, no one can use them against you.",
+ author: "Tyrion Lannister"},
+ {
+    quote: "A day will come when you think yourself safe and happy, and suddenly your joy will turn to ashes in your mouth, and you'll know the debt is paid.",
+ author: "Tyrion Lannister"},
+ {
+    quote: "When you tear out a man’s tongue, you are not proving him a liar, you're only telling the world that you fear what he might say.",
+ author: "Tyrion Lannister"},
+ {
+    quote: "I am malformed, scarred, and small, but... I am made no worse than other men.",
+ author: "Tyrion Lannister"},
+ {
+    quote: "Never forget what you are. The rest of the world will not. Wear it like armor, and it can never be used to hurt you.",
+ author: "Tyrion Lannister"},
+ {
+    quote: "Death is so final, whereas life ... ah, life is full of possibilities.",
+ author: "Tyrion Lannister"},
+ {
+    quote: "We are puppets dancing on the strings of those who came before us, and one day our own children will take up our strings and dance on in our steads.",
+ author: "Tyrion Lannister"},
+ {
+    quote: "Strong as a bear. Fierce as a wolverine. Fear cuts deeper than swords. The man who fears losing has already lost.",
+ author: "Arya Stark"},
+ {
+    quote: "You are Arya of Winterfell, daughter of the North. You told me you could be strong. You have the wolf blood in you.",
+ author: "Arya Stark"},
+ {
+    quote: "Even the sky was grey. The eyes of the bride were brown. Big and brown and full of fear.",
+ author: "Arya Stark"},
+ {
+    quote: "Nothing isn't better or worse than anything. Nothing is just... nothing.",
+ author: "Arya Stark"},
+ {
+    quote: "A long time ago, she remembered her father saying that when the cold winds blow the lone wolf dies but the pack survives. He had it all backward. Arya the lone wolf still lives, but the wolves of the pack had been taken and slain and skinned.",
+ author: "Arya Stark"},
+ {
+    quote: "Melisandre: Brown eyes. Green eyes. And blue eyes [...] What do we say to the god of death? Arya Stark: Not today.",
+ author: "Arya Stark"},
+ {
+    quote: "People will whisper. They’ll make their jokes. Let them.",
+ author: "Cersei Lannister"},
+ {
+    quote: "Love is poison.  A sweet poison, yes, but it will kill you all the same.",
+ author: "Cersei Lannister"},
+ {
+    quote: "Look at me! Look at my face! It’s the last thing you’ll see before you die.",
+ author: "Cersei Lannister"},
+ {
+    quote: "When you play the game of thrones, you win or you die.",
+ author: "Cersei Lannister"},
+ {
+    quote: "Power is power.",
+ author: "Cersei Lannister"},
+ {
+    quote: "The more people you love, the weaker you are. You’ll do things for them that you know you shouldn’t do. You’ll act the fool to make them happy, to keep them safe...Love no one but your children; on that front a mother has no choice.",
+ author: "Cersei Lannister"},
+ {
+    quote: "The gods have no mercy, that’s why they’re gods.",
+ author: "Cersei Lannister"},
+ {
+    quote: "I thought if I could make something so good, so pure, maybe I’m not a monster.",
+ author: "Cersei Lannister"},
+ {
+    quote: "So we fight and die or we submit and die – I know my choice.",
+ author: "Cersei Lannister"},
+ {
+    quote: "Kings have no friends, only subjects and enemies.",
+ author: "Stannis Baratheon"},
+ {
+    quote: "I never asked for this, no more than I asked to be king. Yet dare I disregard her? We do not choose our destinies. Yet we must ... we must do our duty, no? Great or small, we must do our duty.",
+ author: "Stannis Baratheon"},
+ {
+    quote: "Demons made of snow and ice and cold. The ancient enemy. The only enemy that matters.",
+ author: "Stannis Baratheon"},
+ {
+    quote: "I never asked for this crown. Gold is cold and heavy on the head, but so long as I am the king, I have a duty... If I must sacrifice one child to the flames to save a million from the dark... Sacrifice... is never easy, Davos. Or it is no true sacrifice.",
+ author: "Stannis Baratheon"},
+ {
+    quote: "It is not a question of wanting. The throne is mine, as Robert's heir. That is law. After me, it must pass to my daughter, unless Selyse should finally give me a son. I am king. Wants do not enter into it. I have a duty to my daughter. To the realm. Even to Robert. He loved me but little, I know, yet he was my brother. The Lannister woman gave him horns and made a motley fool of him. She may have murdered him as well, as she murdered Jon Arryn and Ned Stark. For such crimes there must be justice. Starting with Cersei and her abominations. But only starting. I mean to scour that court clean. As Robert should have done after the Trident.",
+ author: "Stannis Baratheon"},
+ {
+    quote: "I know the cost! Last night, gazing into that hearth, I saw things in the flames as well. I saw a king, a crown of fire on his brows, burning… burning, Davos. His own crown consumed his flesh and turned him into ash. Do you think I need Melisandre to tell me what that means? Or you?",
+ author: "Stannis Baratheon"},
+ {
+    quote: "Always keep your foes confused, if they don’t know who you are, what you want, they can’t know what you plan to do next.",
+ author: "Petyr Baelish"},
+ {
+    quote: "Only by admitting what we are can we get what we want.",
+ author: "Petyr Baelish"},
+ {
+    quote: "In King's Landing, there are two sorts of people. The players and the pieces.",
+ author: "Petyr Baelish"},
+ {
+    quote: "I learnt that I’ll never win. Not that way. That’s their game, their rules.",
+ author: "Petyr Baelish"},
+ {
+    quote: "Don’t fight in the north or the south. Fight every battle, everywhere, always, in your mind.",
+ author: "Petyr Baelish"},
+ {
+    quote: "In the game of thrones, even the humblest pieces can have wills of their own. Sometimes they refuse to make the moves you've planned for them.",
+ author: "Petyr Baelish"},
+ {
+    quote: "The deed is done. Faded into nothing. Only speaking of it can make it real.",
+ author: "Petyr Baelish"},
+ {
+    quote: "Chaos isn’t a pit. Chaos is a ladder. Many who try to climb it fail, and never get to try again. The fall breaks them.",
+ author: "Petyr Baelish"},
+ {
+    quote: "Don’t worry about your death, worry about your life. Take charge of your life for as long as it lasts.",
+ author: "Petyr Baelish"},
+ {
+    quote: "Distrusting me was the wisest thing you’ve done since you climbed off your horse.",
+ author: "Petyr Baelish"},
+ {
+    quote: "A lion doesn't concern itself with the opinion of sheep.",
+ author: "Tywin Lannister"},
+ {
+    quote: "Any man who must say 'I am king' is no true king at all.",
+ author: "Tywin Lannister"},
+ {
+    quote: "When soldiers lack discipline, the fault lies with their lord commander.",
+ author: "Tywin Lannister"},
+ {
+    quote: "We look like fools and they look like heroes. That’s how kings fall.",
+ author: "Tywin Lannister"},
+ {
+    quote: "The house that puts family first will always defeat the house that puts the whims and wishes of its sons and daughters first.",
+ author: "Tywin Lannister"},
+ {
+    quote: "A king who runs will not be king for long.",
+ author: "Tywin Lannister"},
+ {
+    quote: "I need you to become the man you were always meant to be. Not next year. Not tomorrow. Now.",
+ author: "Tywin Lannister"},
+ {
+    quote: "The only way to keep the small folk loyal is to make certain they fear you more than they do the enemy.",
+ author: "Tywin Lannister"},
+ {
+    quote: "Woman? Is that meant to insult me? I would return the slap if I took you for a man.",
+ author: "Daenerys Targaryen"},
+ {
+    quote: "It seems to me that a queen who trusts no one is as foolish as a queen who trusts everyone.",
+ author: "Daenerys Targaryen"},
+ {
+    quote: "Do you understand? I’m no ordinary woman. My dreams come true.",
+ author: "Daenerys Targaryen"},
+ {
+    quote: "I am not your little princess. I am Daenerys Storm born of the blood of old Valyria and I will take what is mine, with fire and blood I will take it.",
+ author: "Daenerys Targaryen"}, 
+ {
+    quote: "I will not let those I have freed slide back to chains. I will sail for Westeros… I will do what queens do. I will rule.",
+ author: "Daenerys Targaryen"},
+ {
+    quote: "My Watch Has Ended.",
+ author: "Jon Snow"},
+ {
+    quote: "We're Children Playing At A Game, Screaming That The Rules Aren't Fair.",
+ author: "Jon Snow"},
+ {
+    quote: "They Were Born On The Wrong Side Of The Wall - Doesn't Make Them Monsters.",
+ author: "Jon Snow"},
+ {
+    quote: "First Lesson, Stick 'Em With The Pointy End.",
+ author: "Jon Snow"},
+ {
+    quote: "Love Is The Death Of Duty.",
+ author: "Jon Snow"},
+ {
+    quote: "If you would take a man's life, you owe it to him to look into his eyes and hear his final words. And if you cannot bear to do that, then perhaps the man does not deserve to die.",
+ author: "Ned Stark"},
+ {
+    quote: "The man who passes the sentence should swing the sword.",
+ author: "Ned Stark"},
+ {
+    quote: "When the snows fall and the white winds blow, the lone wolf dies, but the pack survives.",
+ author: "Ned Stark"},
+ {
+    quote: "The madness of mercy. That she might save her children.",
+ author: "Ned Stark"},
+ {
+    quote: "You grew up with actors. You learned their craft and you learnt it well. But I grew up with soldiers. I learned to die a long time ago.",
+ author: "Ned Stark"},
+ {
+    quote: "We've come to a dangerous place. We can't afford to fight a war amongst ourselves.",
+ author: "Ned Stark"},
+ {
+    quote: "I'm A Slow Learner, It's True. But I Learn.",
+ author: "Sansa Stark"},
+ {
+    quote: "Your Words Will Disappear. Your House Will Disappear. Your Name Will Disappear. All Memory Of You Will Disappear.",
+ author: "Sansa Stark"},
+ {
+    quote: "You Think He's Going To Fall Into Your Trap, He Won't. He's The One Who Lays Traps.",
+ author: "Sansa Stark"},
+ {
+    quote: "You're Going To Die Tomorrow, Lord Bolton. Sleep Well.",
+ author: "Sansa Stark"},
+ {
+    quote: "He's A Monster.",
+ author: "Sansa Stark"},
+ {
+    quote: "By What Right Does The Wolf Judge The Lion?",
+ author: "Jaime Lannister"},
+ {
+    quote: "She's Always Been Good At Using The Truth To Tell Lies.",
+ author: "Jaime Lannister"},
+ {
+    quote: "Why Have The Gods Made Me Love A Hateful Woman?",
+ author: "Jaime Lannister"},
+ {
+    quote: "The Things I Do For Love.",
+ author: "Jaime Lannister"},
+ {
+    quote: "Oh, The Gods Won't Mind. They've Spilled More Blood Than The Rest Of Us Combined.",
+ author: "Jaime Lannister"},
+
+  {
+    quote: "It's A Strange Thing, The First Time You Cut A Man. You Realize We're Nothing But Sacks Of Meat And Blood, And Some Bone To Keep It All Standing.",
+ author: "Jaime Lannister"},
+  {
+    quote: "My Life Has Left Me Uniquely Unfit For Constraint.",
+ author: "Jaime Lannister"},
+ {
+    quote: "What Is Dead May Never Die.",
+ author: "Theon Greyjoy"},
+ {
+    quote: "I'm Not Fit To Rule.",
+ author: "Theon Greyjoy"},
+ {
+    quote: "My Sword Is Yours, In Victory And Defeat, From This Day Until My Last Day.",
+ author: "Theon Greyjoy"},
+ {
+    quote: "You Gave Me Away Like I Was Some Dog You Didn't Want Anymore, And Now You Curse Me Because I've Come Home.",
+ author: "Theon Greyjoy"},
+ {
+    quote: "Do You Know What It's Like To Be Told How Lucky You Are To Be Someone's Prisoner?",
+ author: "Theon Greyjoy"},
+ {
+    quote: "There's no divine justice. If there was, you'd be dead. And that girl would be alive.",
+ author: "Sandor Clegane"},
+ {
+    quote: "I'm honest. It's the world that's awful.",
+ author: "Sandor Clegane"},
+ {
+    quote: "It is a big and beautiful world. Most of us live and die in the same corner where we were born and never get to see any of it. I don’t want to be most of us.",
+ author: "Oberyn Martell"},
+ {
+    quote: "Today is not the day I die.",
+ author: "Oberyn Martell"}
+];
